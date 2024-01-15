c4a37eddae6375fbf50d768b5236b5bb29344164
uwiduhaye1 <joseuwiduhayee@gmail.com>
2024-01-15 16:28:10 +0200

    non fiction/sinup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import Profiles from './profile/Profiles'
+import non1 from '../public/non1.jpg'
+import non2 from '../public/non2.jpg'
+import non3 from '../public/non3.jpg'
+import non4 from '../public/non4.jpg'
+import non5 from '../public/non5.jpg'
+import non6 from '../public/non6.jpg'
+import non7 from '../public/non7.jpg'
+import non8 from '../public/non8.jpg'
+import non10 from '../public/non10.jpg'
+import non11 from '../public/non11.jpg'
+import non12 from '../public/non12.jpg'
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+              <Profiles image={non1} name="Maya Angelou" description= "Maya Angelou (April 4, 1928 – May 28, 2014) was an American poet"  />
+             
+        <Profiles image={non2} name="The Boys in the Boat" description=" the Boys in the Boat is a non-fic2013. ."/>
+                                                                               
+        <Profiles image={non3} name="Other Slavery" description="This novel explores the complex relationships between slaves"  />
+                                                                                                                                    
+        <Profiles image={non4} name="The Nonfiction Family Tree" description= " their family history.and methods for building." />
+        <Profiles image={non5} name="The Dairly of A young Girl" description= "This poignant and influential diary is the ."/>
+         <Profiles image={non6} name="Maya Angelou" description= "Maya Angelou (April 4, 1928 – May 28, 2014) " />
+         <Profiles image={non7} name="All things Bright and Beatiful"description= " recount his experiences with a wide" />
+         <Profiles image={non8} name="in the Heart of the Sea" description= "the harrowing true story of the whal Moby-Dick." />
+         <Profiles image={non10} name="The Bug of girl" description= " booklife of Maria Merian, a pioneering naturalist" />
+         <Profiles image={non11} name="Ghost Of Tsunami" description= " is a recent or niche publication, I recommend checking with local " />
+         <Profiles image={non12} name="Becomming of Michelle Obama" description= " from her childhood in the South Side " />
+         <button className='btnlibrary'>add new book</button>
+         
+    
+       
     </>
-  )
+  );
 }
 
 export default App
+
