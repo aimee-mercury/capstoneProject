a109550dbe5d9de97779b5402042cab7e1342d74
marie <ingabiremarieaimee6@gmail.com>
2024-01-19 15:34:04 -0800

    my work was to make navigation fixed and also make responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Support from './Supportusform/Supportform';
 import Nonfiction from './Non-Fiction/Nonfiction';
 import Fiction from './Fiction/Fiction';
 import Home from './components/Home';
+import About from './components/About';
 import Categories from './Categories/Categories';
 import Supportus from './Support Us/supportus';
-
 function App() {
  
 
   return (
     <>
-  
+
   <Router>
     <Routes>
-
       <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
       <Route path='/Categories' element={<Categories/>}/>
       <Route path='/Support' element={<Supportus/>}/>
       <Route path='/signup' element={<Signup/>}/>
