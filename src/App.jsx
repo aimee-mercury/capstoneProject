513c22b69cd5bd0c438e2f51678a7c3678084677
mutonirachel <mutonirachel20@gmail.com>
2024-01-19 17:29:07 +0200

    some css and fiction & non-fiction books pages again

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-
-import SinUp from './Sinupproject/sinup';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './login/login';
-import Support from './Supportusform/Supportform';
-import Home from './home';
-import './App.css'
-import Nonfiction from './Non-Fiction/Nonfiction';
-import Fiction from './Fiction/Fiction';
-
-
-=======
 import Signup from './Sinupproject/sinup'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login/login';
@@ -20,19 +7,13 @@ import Fiction from './Fiction/Fiction';
 import Home from './components/Home';
 import Categories from './Categories/Categories';
 import Supportus from './Support Us/supportus';
->>>>>>> cd0312bdf04923032898afa4ca669cb28f0fc806
 
 function App() {
  
 
   return (
     <>
-<<<<<<< HEAD
-
-
-=======
   
->>>>>>> cd0312bdf04923032898afa4ca669cb28f0fc806
   <Router>
     <Routes>
 
