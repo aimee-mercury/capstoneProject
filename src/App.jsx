634b7f5e3ab76cd0c2b63b4329d98797577b0058
mutonirachel <mutonirachel20@gmail.com>
2024-01-26 10:17:01 +0200

    new dashboard with all pages again

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Category from './Categories/Categories';
 import Supportus from './Support Us/supportus';
 import SinUp from './Sinupproject/sinup';
 import Visit from './visit/Visit';
-<<<<<<< HEAD
 import Books from './Dashboardd/Books'
 import Finance from './Dashboardd/Finance'
 import Help from './Dashboardd/Help'
@@ -17,22 +16,11 @@ import Settings from './Dashboardd/Settings'
 import Signout from './Dashboardd/Signout'
 import Dash from './Dashboardd/Dash'
 import Members from './Dashboardd/Members'
-
-=======
-import Dashb from './Dashboard/Dashb';
-import Book from './Dashboard/Book';
-import Members from './Dashboard/Members';
-import Settings from './Dashboard/Settings';
-import Finance from './Dashboard/Finance';
-import Help from './Dashboard/Help';
-
 import SearchResultsPage from './components/SearchResultsPage';
-
 import Fiction from './Fiction/Fiction';
 import Nonfiction from './Non-Fiction/Nonfiction';
 import Academics from './profile/Academics/Academic';
 import Bibiliograph from './profile/Biography/Biogr';
->>>>>>> db2846e5f885c019f467134b91ee1ff4928ca70c
 
 function App() {
  
